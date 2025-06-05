b1c9bc5a880843f0267105fdec2ccf1c8db143d0
Brianna Flores <briannaflores@meta.com>
2025-06-04 20:32:08 -0700

    added functionality to dynamically create playlist cards

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,58 @@
+let playlists = [
+	{
+        "playlistID": 3,
+        "playlist_name": "Lo-Fi Chill",
+        "playlist_author": "Various Artists",
+        "playlist_art": "assets/img/playlist.png",
+        "songs": 24
+    },
+    {
+        "playlistID": 4,
+        "playlist_name": "Disney Jams",
+        "playlist_author": "Walt Disney",
+        "playlist_art": "assets/img/playlist.png",
+        "songs": 15
+    },
+    {
+        "playlistID": 5,
+        "playlist_name": "Morning Energy",
+        "playlist_author": "George Michael",
+        "playlist_art": "assets/img/playlist.png",
+        "songs": 18
+    },
+    {
+        "playlistID": 6,
+        "playlist_name": "Jazz Essentials",
+        "playlist_author": "Chet Baker",
+        "playlist_art": "assets/img/playlist.png",
+        "songs": 10
+    },
+    {
+        "playlistID": 7,
+        "playlist_name": "Late Night Drive",
+        "playlist_author": "Daniel Caesar",
+        "playlist_art": "assets/img/playlist.png",
+        "songs": 14
+    },
+    {
+        "playlistID": 8,
+        "playlist_name": "Summer Jams",
+        "playlist_author": "Phineas and Ferb",
+        "playlist_art": "assets/img/playlist.png",
+        "songs": 20
+    },
+    {
+        "playlistID": 9,
+        "playlist_name": "Throwback Jams",
+        "playlist_author": "Rick Astley",
+        "playlist_art": "assets/img/playlist.png",
+        "songs": 13
+    },
+    {
+        "playlistID": 10,
+        "playlist_name": "Coffee",
+        "playlist_author": "Laufey",
+        "playlist_art": "assets/img/playlist.png",
+        "songs": 16
+    }
+];
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -46,5 +46,48 @@ window.onclick = function(event) {
 
 
 // iterate over data.playlists array
+const loadPlaylists = () => {
+    if (playlists.length === 0) {
+        const mainBody = document.querySelector("main");
+        mainBody.innerHTML = `
+            <p>No playlists have been added</p>
+        `;
+    }
+    let count = 0;
+    const container = document.getElementById('playlist-cards');
+    let row;
+    for (const playlist of playlists) {
+        if (count % 4 === 0) {
+            row = document.createElement('section');
+            row.className = 'card-row';
+            container.appendChild(row);
+        }
+        // create a new card
+        let newCard = createCard(playlist);
+        // add card to container
+        row.appendChild(newCard);
+        count++;
+    }
+}
 
 // create card for each playlist
+const createCard = (playlist) => {
+    const playlistElement = document.createElement('section');
+    playlistElement.className = "card outline";
+    playlistElement.onclick = function() {openModal()};
+    playlistElement.innerHTML = `
+        <img class="outline" id="playlist-photo" src='${playlist.playlist_art}'/>
+        <h3 class="no-space">${playlist.playlist_name}</h3>
+        <p class="no-space">${playlist.playlist_author}</p>
+        <section class="likes no-space">
+            <img class="outline" id="heart" />
+            <p id="like-count">likes</p>
+        </section>
+    `;
+    return playlistElement;
+}
+
+// execute only once all content loaded
+document.addEventListener("DOMContentLoaded", () => {  // we use an arrow function so we can execute multiple functions
+    loadPlaylists();
+});
